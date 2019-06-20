4f6d56374c8162f0ccf80f29009077058a161aa4
alexNeto <alex.neto3@hotmail.com>
2019-06-20 15:57:28 -0300

    feito card para previsao do tempo

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 
+
+
+
 const materialModules = [
   MatInputModule,
   MatCardModule,
+  MatGridListModule,
   MatButtonModule,
+  MatBadgeModule,
   MatListModule,
   MatToolbarModule,
   MatAutocompleteModule
