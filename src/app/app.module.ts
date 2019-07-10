8ff8c4372a943990ea40e316358cd463c458470f
alexNeto <alex.neto3@hotmail.com>
2019-07-10 00:48:42 -0300

    #5 feito layout para compoenente de voto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { ToggleComponent } from './components/toggle/toggle.component';
 import { HeaderComponent } from './header/header.component';
 import { ToolPanelComponent } from './tool-panel/tool-panel.component';
-import { ToggleComponent } from './components/toggle/toggle.component';
+import { VoteComponent } from './components/vote/vote.component';
+import { UpVoteComponent } from './svg/up-vote/up-vote.component';
+import { DownVoteComponent } from './svg/down-vote/down-vote.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +19,10 @@ import { ToggleComponent } from './components/toggle/toggle.component';
     HeaderComponent,
     ToolPanelComponent,
     ToggleComponent,
+    SearchComponent,
+    VoteComponent,
+    UpVoteComponent,
+    DownVoteComponent,
   ],
   imports: [
     HttpClientModule,
