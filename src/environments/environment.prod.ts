b56725998a3b5d79c9fd1fd3256517a8120c8dc0
alexNeto <alex.neto3@hotmail.com>
2019-08-28 02:47:00 -0300

    migrated to app engine

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://lada-api.herokuapp.com',
+  baseUrl: 'https://lada-api-250821.appspot.com',
   sentryDsn: 'https://41cb73ac3c864071bf33576f91925216@sentry.io/1528979'
 };
