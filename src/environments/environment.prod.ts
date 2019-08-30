c0af898518db6bb66ba7e1c32bf500b887612ad9
alexNeto <alex.neto3@hotmail.com>
2019-08-30 19:12:33 -0300

    change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://lada-api-250821.appspot.com',
+  baseUrl: 'https://api.v1.lada-app.com',
   sentryDsn: 'https://41cb73ac3c864071bf33576f91925216@sentry.io/1528979'
 };
